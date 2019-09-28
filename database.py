e8a804aea59808837eb4720d7400811a51455b75
Amr Elkady <amr.elkady153@gmail.com>
2019-09-28 23:19:59 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.insert(0, "/var/www/directme/Directme/venv3/lib/python3.5/site-packages") 
 import os
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
